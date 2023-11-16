f243f6da084be62e90d318d57aeb138198d47522
tdeerenberg <113618658+tdeerenberg@users.noreply.github.com>
2023-11-16 21:09:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 # Make sure to add ~/.local/bin to $PATH
 pip3 install -r requirements.txt
 cp src/musort.py ~/.local/bin/musort
-echo "IF NOT DONE ALREADY, ADD '~/local/bin' TO $ PATH"
+echo "IF NOT DONE ALREADY, ADD '~/.local/bin' TO $ PATH"
